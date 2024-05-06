16370adf46a434ada98181461e854c3d8511455b
JUYONG0133 <qwer99633@naver.com>
2024-05-06 14:27:47 +0900

    0505

---
+++ javawork/naver0314javaproject/JspProject/src/main/java/data/dto/CaroptionDto.java
@@ -0,0 +1,77 @@
+package data.dto;
+
+public class caroption {
+
+    private int num;
+    private String userid;
+    private String carname;
+    private String wheel;
+    private String nav;
+    private String carkey;
+    private String caroption;
+    private String etc;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getCarname() {
+        return carname;
+    }
+
+    public void setCarname(String carname) {
+        this.carname = carname;
+    }
+
+    public String getWheel() {
+        return wheel;
+    }
+
+    public void setWheel(String wheel) {
+        this.wheel = wheel;
+    }
+
+    public String getNav() {
+        return nav;
+    }
+
+    public void setNav(String nav) {
+        this.nav = nav;
+    }
+
+    public String getCarkey() {
+        return carkey;
+    }
+
+    public void setCarkey(String carkey) {
+        this.carkey = carkey;
+    }
+
+    public String getCaroption() {
+        return caroption;
+    }
+
+    public void setCaroption(String caroption) {
+        this.caroption = caroption;
+    }
+
+    public String getEtc() {
+        return etc;
+    }
+
+    public void setEtc(String etc) {
+        this.etc = etc;
+    }
+}
