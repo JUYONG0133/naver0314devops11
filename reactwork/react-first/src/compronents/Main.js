6a154507843e6030e5f55468e37bdf87eb4c8dbe
JUYONG0133 <qwer99633@naver.com>
2024-07-09 09:42:17 +0900

    react study day 0708

--- reactwork/react-first/src/compronents/Main.js
+++ reactwork/react-first/src/compronents/Main.js
@@ -7,25 +7,40 @@ import ThreeApp from "./ThreeApp";
 import FourApp from "./FourApp";
 import FiveApp from "./FiveApp";
 import SixApp from "./SixApp";
+import {useState} from "react";
 
 const Main=()=>{
+    const [idx,setIdx]=useState(6);
     return(
 
         <div>
-            <SixApp/>
-            <hr/>
-            <FiveApp/>
-            <hr/>
-            <FourApp/>
-            <hr/>
-            <ThreeApp/>
-            <hr/>
-            <TwoApp/>
-            <hr/>
-            <OneApp />
-            <hr />
+            {/*<SixApp/>*/}
+            {/*<hr/>*/}
+            {/*<FiveApp/>*/}
+            {/*<hr/>*/}
+            {/*<FourApp/>*/}
+            {/*<hr/>*/}
+            {/*<ThreeApp/>*/}
+            {/*<hr/>*/}
+            {/*<TwoApp/>*/}
+            {/*<hr/>*/}
+            {/*<OneApp />*/}
+            {/*<hr />*/}
 
-
+        <h1 >24/07/08 react 첫 수업</h1>
+            <select className="form-select" style={{width:'200px'}}
+            onChange={(e)=>setIdx(Number(e.target.value))}>
+                <option value={1}>1번 컴포넌트</option>
+                <option value={2}>2번 컴포넌트</option>
+                <option value={3}>3번 컴포넌트</option>
+                <option value={4}>4번 컴포넌트</option>
+                <option value={5}>5번 컴포넌트</option>
+                <option selected value={6}>6번 컴포넌트</option>
+            </select>
+            <br/><br/>
+            {
+                idx===1?<OneApp/>:idx===2?<TwoApp/>:idx===3?<ThreeApp/>:idx===4?<FourApp/>:idx===5?<FiveApp/>:<SixApp/>
+            }
         </div>
     )
 }
